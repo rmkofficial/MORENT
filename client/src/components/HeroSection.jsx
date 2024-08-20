cc7cae767b95c568b9520baacf86235718bbd091
rmkofficial <kmert0@outlook.com>
2024-08-20 13:26:16 +0300

    Update HeroSection component layout and styles

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ function HeroSection() {
       <div className="flex flex-col items-center sm:flex-row sm:justify-center space-y-4 sm:space-y-0 sm:space-x-2">
         {/* Kart 1 */}
         <div className="bg-blue-600 text-white p-6 rounded-lg flex flex-col justify-between relative w-full sm:w-3/5 h-96">
-          <div className="text-left max-w-[50%]">
+          <div className="text-left w-full sm:max-w-[50%]">
             <h2 className="text-2xl font-bold mb-2">
               The Best Platform for Car Rental
             </h2>
@@ -22,11 +22,11 @@ function HeroSection() {
           <img
             src={car1}
             alt="Car 1"
-            className="absolute bottom-6 left-1/2 transform -translate-x-1/2 w-96 h-auto"
+            className="absolute bottom-6 left-1/2 transform -translate-x-1/2 w-48 sm:w-96 h-auto"
           />
         </div>
         {/* Kart 2 */}
-        <div className="bg-blue-500 text-white p-6 rounded-lg flex flex-col justify-between relative w-full sm:w-3/5 h-96">
+        <div className="hidden sm:flex bg-blue-500 text-white p-6 rounded-lg flex-col justify-between relative w-full sm:w-3/5 h-96">
           <div className="text-left max-w-[50%]">
             <h2 className="text-2xl font-bold mb-2">
               Easy way to rent a car at a low price
