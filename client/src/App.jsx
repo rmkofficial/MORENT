78366bb8b03adf2f7b81f8b7515916fcd3ffdb77
rmkofficial <kmert0@outlook.com>
2024-09-15 11:02:48 +0300

    Refactor CSS and button styles in multiple components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
 import { Provider } from "react-redux";
-import { store } from "./store/store"; // Redux store'u import et
-import HomePage from "./pages/HomePage"; // HomePage import et
+import { store } from "./store/store";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Provider store={store}>
-      {" "}
-      {/* Redux Provider ile tüm uygulamayı sarmala */}
-      <div>
-        <HomePage /> {/* Tüm sayfayı sarmalayan HomePage */}
-      </div>
+      <HomePage />
     </Provider>
   );
 }
